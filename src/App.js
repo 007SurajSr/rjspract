700e5ad52459eeb09bae5c2b5469e8c3b5c8826f
Suraj Gupta <8700suraj@gmail.com>
2023-06-05 20:00:13 +0530

    Counter2.0

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React, {useState} from "react";
+ 
 
 function App() {
-  const list = [
-    "Banana",
-    "Apple",
-    "Orange",
-    "Mango",
-    "Pineapple",
-    "Watermelon"
-  ];
-
-  const [filterList, setFilterList] = useState(list);
-
-  const handleSearch = (event) => {
-
-    if (event.target.value === "") {
-      setFilterList(list);
-      return;
-    }
-    const filteredValues = list.filter(
-      (item) =>
-        item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
-    );
-    setFilterList(filteredValues);
-  };
-  return (
+  const[count,setCount] = useState(0);
+ 
+  return(
     <div className="app">
-      <div>
-        Search: <input name="query" type="text" onChange={handleSearch} />
+      <h1 className={count > 0 ? "positive" : count > 0 ? "negative" : null}>{count}</h1>
+      <div className='button-wrapper'>
+        <button onClick={()=>setCount(count-1)}>-</button>
+        <button onClick={()=>setCount(count+1)}>+</button>
       </div>
-      {filterList &&
-        filterList.map((item, index) => (
-          <div key={index}>{item}</div> //Display each item
-        ))}
     </div>
+  
+  
+  
+  // const list = [
+  //   "Banana",
+  //   "Apple",
+  //   "Orange",
+  //   "Mango",
+  //   "Pineapple",
+  //   "Watermelon"
+  // ];
+
+  // const [filterList, setFilterList] = useState(list);
+
+  // const handleSearch = (event) => {
+
+  //   if (event.target.value === "") {
+  //     setFilterList(list);
+  //     return;
+  //   }
+  //   const filteredValues = list.filter(
+  //     (item) =>
+  //       item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+  //   );
+  //   setFilterList(filteredValues);
+  // };
+  // return (
+  //   <div className="app">
+  //     <div>
+  //       Search: <input name="query" type="text" onChange={handleSearch} />
+  //     </div>
+  //     {filterList &&
+  //       filterList.map((item, index) => (
+  //         <div key={index}>{item}</div> //Display each item
+  //       ))}
+  //   </div>
   );
 }
 
