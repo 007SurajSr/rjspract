ff539398c6a782caa7cf4a6a6218a081f892acba
Suraj Gupta <8700suraj@gmail.com>
2023-06-06 15:22:20 +0530

    counter2.0

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
  
   return(
     <div className="app">
-      <h1 className={count > 0 ? "positive" : count > 0 ? "negative" : null}>{count}</h1>
-      <div className='button-wrapper'>
+      <h1 className={count > 0 ? "positive" : count < 0 ? "negative" : null}>{count}</h1>
+      <div className='button__wrapper'>
         <button onClick={()=>setCount(count-1)}>-</button>
         <button onClick={()=>setCount(count+1)}>+</button>
       </div>
